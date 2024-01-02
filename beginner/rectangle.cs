3f26360bebc1a772c6dff333d16500dc2b6f88bd
kadin <kadinvo@gmail.com>
2024-01-02 15:05:06 +1300

    Learnt dobule  and conversion

---
+++ beginner/rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Rectangle
+{
+    static void Main()
+    {
+        // Prompt user for length
+        Console.Write("Enter the length of the rectangle: ");
+        double length = Convert.ToDouble(Console.ReadLine());
+
+        // Prompt user for width
+        Console.Write("Enter the width of the rectangle: ");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        // Calculate area
+        double area = length * width;
+
+      
+        Console.WriteLine($"The area of the rectangle is: {area}");
+
+   
+        Console.ReadLine();
+    }
+}
