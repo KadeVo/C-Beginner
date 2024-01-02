c1977a0e6b93804e5ee5ddb8ffcbe861ed1e8df3
kadin <kadinvo@gmail.com>
2024-01-02 14:56:17 +1300

    Learnt input, writeline and readline

--- beginner/Program.cs
+++ beginner/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        // Prompt user for their name
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+
+        // Greet the user
+        Console.WriteLine($"Hello, {userName}! Welcome to C# programming.");
+
+        // Pause to keep console window open
+        Console.ReadLine();
+    }
+}
