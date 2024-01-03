2df29d23dfa5143457f3d76a16c7d3ccc489ef68
kadin <kadinvo@gmail.com>
2024-01-03 16:29:25 +1300

    Created discount calculator

---
+++ PercentageCalc/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class DiscountCalculator
+{
+    static void Main()
+    {
+        
+        Console.Write("Enter the original price of the item: ");
+        double originalPrice = double.Parse(Console.ReadLine());
+
+     
+        Console.Write("Enter the discount percentage (e.g., 25 for 25%): ");
+        double discountPercent = double.Parse(Console.ReadLine()) / 100; 
+        
+        double discountAmount = originalPrice * discountPercent;
+
+      
+        double finalPrice = originalPrice - discountAmount;
+
+     
+        Console.WriteLine("Original price: ${0:0.00}", originalPrice);
+        Console.WriteLine("Discount amount: ${0:0.00}", discountAmount);
+        Console.WriteLine("Final price: ${0:0.00}", finalPrice);
+
+        Console.ReadLine(); 
+    }
+}
