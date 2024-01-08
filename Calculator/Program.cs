ca48fb118441988cac97e30bcc2f3934709a3ffd
kadin <kadinvo@gmail.com>
2024-01-08 15:13:01 +1300

    Simple calculator in C#

---
+++ Calculator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Rectangle
+{
+    static void Main()
+    {
+        double num1 = 0;
+        double num2 = 0;
+        double result = 0;
+
+        
+        Console.WriteLine("Enter your first number: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+
+        Console.WriteLine("Enter your second number: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter an symbol: ");
+        Console.WriteLine(("+ : Addition"));
+        Console.WriteLine(("- : Subtraction"));
+        Console.WriteLine(("/ : Division"));
+        Console.WriteLine(("* : Multiplication"));
+
+
+        switch(Console.ReadLine())
+        {
+            case "+":
+                result = num1 + num2;
+                Console.WriteLine(result); 
+                break;
+            case "-":
+                result = num1 - num2;
+                Console.WriteLine(result);
+            break;
+            case "*":
+                result = num1 * num2;
+                Console.WriteLine(result);
+                break;
+            case "/":
+                result = num1 / num2;
+                Console.WriteLine(result);
+                break;
+            default: Console.WriteLine("Not a valid Symbol");
+                break;
+
+        }
+        Console.ReadLine();
+    }
+}
