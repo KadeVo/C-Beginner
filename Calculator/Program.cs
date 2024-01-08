d919ee871e7ae2e3bb2be735396ec1b5456d5e6c
kadin <kadinvo@gmail.com>
2024-01-08 15:34:04 +1300

    Created a while loop for the calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-class Rectangle
+class Calculator
 {
     static void Main()
     {
@@ -8,43 +8,57 @@ class Rectangle
         double num2 = 0;
         double result = 0;
 
-        
-        Console.WriteLine("Enter your first number: ");
-        num1 = Convert.ToDouble(Console.ReadLine());
-
-
-        Console.WriteLine("Enter your second number: ");
-        num2 = Convert.ToDouble(Console.ReadLine());
-
-        Console.WriteLine("Enter an symbol: ");
-        Console.WriteLine(("+ : Addition"));
-        Console.WriteLine(("- : Subtraction"));
-        Console.WriteLine(("/ : Division"));
-        Console.WriteLine(("* : Multiplication"));
-
-
-        switch(Console.ReadLine())
+        do
         {
-            case "+":
-                result = num1 + num2;
-                Console.WriteLine($"Result: {num1} + {num2} = {result}");
-                break;
-            case "-":
-                result = num1 - num2;
-                Console.WriteLine($"Result: {num1} - {num2} = {result}");
-                break;
-            case "*":
-                result = num1 * num2;
-                Console.WriteLine($"Result: {num1} * {num2} = {result}");
-                break;
-            case "/":
-                result = num1 / num2;
-                Console.WriteLine($"Result: {num1} / {num2} = {result}");
-                break;
-            default: Console.WriteLine("Not a valid Symbol");
-                break;
+            Console.WriteLine("Enter your first number: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
 
-        }
+
+            Console.WriteLine("Enter your second number: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter an symbol: ");
+            Console.WriteLine(("+ : Addition"));
+            Console.WriteLine(("- : Subtraction"));
+            Console.WriteLine(("/ : Division"));
+            Console.WriteLine(("* : Multiplication"));
+
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine($"Result: {num1} + {num2} = {result}");
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine($"Result: {num1} - {num2} = {result}");
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine($"Result: {num1} * {num2} = {result}");
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    Console.WriteLine($"Result: {num1} / {num2} = {result}");
+                    break;
+                default:
+                    Console.WriteLine("Not a valid Symbol");
+                    break;
+
+            }
+            Console.WriteLine("Enter 'done' to finish or enter anything else to continue:");
+            string userChoice = Console.ReadLine();
+
+            if (userChoice.ToLower() == "done")
+            {
+                Console.WriteLine("Program closing...");
+                break;
+            }
+        } while (true);
+
         Console.ReadLine();
+
+
     }
-}
+    }
